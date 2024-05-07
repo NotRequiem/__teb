5ff8a22c750df24b5da397f439c7bfa45d3cfeb1
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-05-08 00:32:15 +0200

    Add files via upload

---
+++ src/scn/_TIB.c
@@ -0,0 +1,286 @@
+﻿#include <stdio.h>
+#include "struct.h"
+
+/*
+static void GuidToString(const GUID* guid, char** str) {
+    int bufferSize = snprintf(NULL, 0, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+        guid->Data1, guid->Data2, guid->Data3,
+        guid->Data4[0], guid->Data4[1], guid->Data4[2], guid->Data4[3],
+        guid->Data4[4], guid->Data4[5], guid->Data4[6], guid->Data4[7]);
+
+    *str = (char*)malloc(bufferSize + 1);
+
+    if (*str != NULL) {
+        sprintf_s(*str, bufferSize + 1, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+            guid->Data1, guid->Data2, guid->Data3,
+            guid->Data4[0], guid->Data4[1], guid->Data4[2], guid->Data4[3],
+            guid->Data4[4], guid->Data4[5], guid->Data4[6], guid->Data4[7]);
+    }
+    else {
+        printf("Memory allocation failed.\n");
+    }
+}
+*/
+
+void GuidToString(const GUID* guid, char* str) {
+    sprintf_s(str, 39, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+        guid->Data1, guid->Data2, guid->Data3,
+        guid->Data4[0], guid->Data4[1], guid->Data4[2], guid->Data4[3],
+        guid->Data4[4], guid->Data4[5], guid->Data4[6], guid->Data4[7]);
+}
+
+void printTEBFields(PTEB pTEB) {
+    printf("NtTib: %p\n", &pTEB->NtTib);
+    printf("EnvironmentPointer: %p\n", pTEB->EnvironmentPointer);
+    printf("ClientId.UniqueProcess: %p\n", pTEB->ClientId.UniqueProcess);
+    printf("ClientId.UniqueThread: %p\n", pTEB->ClientId.UniqueThread);
+    printf("ActiveRpcHandle: %p\n", pTEB->ActiveRpcHandle);
+    printf("ThreadLocalStoragePointer: %p\n", pTEB->ThreadLocalStoragePointer);
+    printf("ProcessEnvironmentBlock: %p\n", pTEB->ProcessEnvironmentBlock);
+    printf("LastErrorValue: %u\n", pTEB->LastErrorValue);
+    printf("CountOfOwnedCriticalSections: %u\n", pTEB->CountOfOwnedCriticalSections);
+    printf("CsrClientThread: %p\n", pTEB->CsrClientThread);
+    printf("Win32ThreadInfo: %p\n", pTEB->Win32ThreadInfo);
+    printf("User32Reserved: ");
+    for (int i = 0; i < 26; i++) {
+        printf("%u ", pTEB->User32Reserved[i]);
+    }
+    printf("\n");
+    printf("UserReserved: ");
+    for (int i = 0; i < 5; i++) {
+        printf("%u ", pTEB->UserReserved[i]);
+    }
+    printf("\n");
+    printf("WOW32Reserved: %p\n", pTEB->WOW32Reserved);
+    printf("CurrentLocale: %u\n", pTEB->CurrentLocale);
+    printf("FpSoftwareStatusRegister: %u\n", pTEB->FpSoftwareStatusRegister);
+    printf("ReservedForDebuggerInstrumentation: ");
+    for (int i = 0; i < 16; i++) {
+        printf("%p ", pTEB->ReservedForDebuggerInstrumentation[i]);
+    }
+    printf("\n");
+#ifdef _WIN64
+    printf("SystemReserved1: ");
+    for (int i = 0; i < 30; i++) {
+        printf("%p ", pTEB->SystemReserved1[i]);
+    }
+    printf("\n");
+#else
+    printf("SystemReserved1: ");
+    for (int i = 0; i < 26; i++) {
+        printf("%p ", pTEB->SystemReserved1[i]);
+    }
+    printf("\n");
+#endif
+    printf("PlaceholderCompatibilityMode: %c\n", pTEB->PlaceholderCompatibilityMode);
+    printf("PlaceholderHydrationAlwaysExplicit: %d\n", pTEB->PlaceholderHydrationAlwaysExplicit);
+    printf("PlaceholderReserved: ");
+    for (int i = 0; i < 10; i++) {
+        printf("%c ", pTEB->PlaceholderReserved[i]);
+    }
+    printf("\n");
+    printf("ProxiedProcessId: %u\n", pTEB->ProxiedProcessId);
+    printf("ActivationStack: %p\n", &pTEB->ActivationStack);
+    printf("WorkingOnBehalfTicket: ");
+    for (int i = 0; i < 8; i++) {
+        printf("%u ", pTEB->WorkingOnBehalfTicket[i]);
+    }
+    printf("\n");
+    printf("ExceptionCode: %u\n", pTEB->ExceptionCode);
+    printf("ActivationContextStackPointer: %p\n", pTEB->ActivationContextStackPointer);
+    printf("InstrumentationCallbackSp: %llu\n", pTEB->InstrumentationCallbackSp);
+    printf("InstrumentationCallbackPreviousPc: %llu\n", pTEB->InstrumentationCallbackPreviousPc);
+    printf("InstrumentationCallbackPreviousSp: %llu\n", pTEB->InstrumentationCallbackPreviousSp);
+#ifdef _WIN64
+    printf("TxFsContext: %lu\n", pTEB->TxFsContext);
+#endif
+    printf("InstrumentationCallbackDisabled: %d\n", pTEB->InstrumentationCallbackDisabled);
+#ifndef _WIN64
+    printf("SpareBytes: ");
+    for (int i = 0; i < 23; i++) {
+        printf("%u ", pTEB->SpareBytes[i]);
+    }
+    printf("\n");
+    printf("TxFsContext: %lu\n", pTEB->TxFsContext);
+
+    printf("UnalignedLoadStoreExceptions: %d\n", pTEB->UnalignedLoadStoreExceptions);
+#endif
+    printf("GdiTebBatch: %p\n", &pTEB->GdiTebBatch);
+    printf("RealClientId.UniqueProcess: %p\n", pTEB->RealClientId.UniqueProcess); // normally this is the same as ClientId.UniqueProcess
+    printf("RealClientId.UniqueThread: %p\n", pTEB->RealClientId.UniqueThread); // normally this is the same as ClientId.UniqueThread
+    printf("GdiCachedProcessHandle: %p\n", pTEB->GdiCachedProcessHandle);
+    printf("GdiClientPID: %lu\n", pTEB->GdiClientPID);
+    printf("GdiClientTID: %lu\n", pTEB->GdiClientTID);
+    printf("GdiThreadLocalInfo: %p\n", pTEB->GdiThreadLocalInfo);
+    printf("Win32ClientInfo: ");
+    for (int i = 0; i < WIN32_CLIENT_INFO_LENGTH; i++) {
+        printf("%lu ", (unsigned long)pTEB->Win32ClientInfo[i]);
+    }
+    printf("\n");
+    printf("glDispatchTable: ");
+    for (int i = 0; i < 233; i++) {
+        printf("%p ", pTEB->glDispatchTable[i]);
+    }
+    printf("\n");
+    printf("glReserved1: ");
+    for (int i = 0; i < 29; i++) {
+        printf("%llu ", pTEB->glReserved1[i]);
+    }
+    printf("\n");
+    printf("glReserved2: %p\n", pTEB->glReserved2);
+    printf("glSectionInfo: %p\n", pTEB->glSectionInfo);
+    printf("glSection: %p\n", pTEB->glSection);
+    printf("glTable: %p\n", pTEB->glTable);
+    printf("glCurrentRC: %p\n", pTEB->glCurrentRC);
+    printf("glContext: %p\n", pTEB->glContext);
+    printf("LastStatusValue: %u\n", pTEB->LastStatusValue);
+    printf("StaticUnicodeString: %p\n", &pTEB->StaticUnicodeString);
+    printf("StaticUnicodeBuffer: "); // this might not print any buffer
+    for (int i = 0; i < STATIC_UNICODE_BUFFER_LENGTH; i++) {
+        printf("%lc ", pTEB->StaticUnicodeBuffer[i]);
+    }
+    printf("\n");
+    printf("DeallocationStack: %p\n", pTEB->DeallocationStack);
+    printf("TlsSlots:\n");
+    for (int i = 0; i < TLS_MINIMUM_AVAILABLE; i++) {
+        void* data_ptr = (void*)pTEB->TlsSlots[i];
+        printf("Slot %d: %p\n", i, data_ptr);
+    }
+    printf("TlsLinks: %p\n", &pTEB->TlsLinks);
+    printf("Vdm: %p\n", pTEB->Vdm);
+    printf("ReservedForNtRpc: %p\n", pTEB->ReservedForNtRpc);
+    printf("DbgSsReserved: ");
+    for (int i = 0; i < 2; i++) {
+        printf("%p ", pTEB->DbgSsReserved[i]);
+    }
+    printf("\n");
+    printf("HardErrorMode: %lu\n", pTEB->HardErrorMode);
+#ifdef _WIN64
+    printf("Instrumentation: ");
+    for (int i = 0; i < 11; i++) {
+        printf("%p ", pTEB->Instrumentation[i]);
+    }
+    printf("\n");
+#else
+    printf("Instrumentation: ");
+    for (int i = 0; i < 9; i++) {
+        printf("%p ", pTEB->Instrumentation[i]);
+    }
+    printf("\n");
+#endif
+    char activityIdStr[40] = { 0 };
+    GuidToString(&pTEB->ActivityId, activityIdStr);
+    printf("ActivityId: %s\n", activityIdStr);    
+    printf("SubProcessTag: %p\n", pTEB->SubProcessTag);
+    printf("PerflibData: %p\n", pTEB->PerflibData);
+    printf("EtwTraceData: %p\n", pTEB->EtwTraceData);
+    printf("WinSockData: %p\n", pTEB->WinSockData);
+    printf("GdiBatchCount: %lu\n", pTEB->GdiBatchCount);
+    if (pTEB->CurrentIdealProcessor.Group != 0 || pTEB->CurrentIdealProcessor.Number != 0 || pTEB->CurrentIdealProcessor.Reserved != 0) {
+        printf("CurrentIdealProcessor: Group=%hu, Number=%hhu, Reserved=%hhu\n",
+            pTEB->CurrentIdealProcessor.Group, pTEB->CurrentIdealProcessor.Number, pTEB->CurrentIdealProcessor.Reserved);
+    }
+    else {
+        printf("CurrentIdealProcessor: Group=%hu, Number=%hhu, Reserved=%hhu\n",
+            pTEB->CurrentIdealProcessor.Group,
+            pTEB->CurrentIdealProcessor.Number,
+            pTEB->CurrentIdealProcessor.Reserved);
+    }
+    printf("IdealProcessorValue: %lu\n", pTEB->IdealProcessorValue);
+    printf("ReservedPad0: %hhu\n", pTEB->ReservedPad0);
+    printf("ReservedPad1: %hhu\n", pTEB->ReservedPad1);
+    printf("ReservedPad2: %hhu\n", pTEB->ReservedPad2);
+    printf("IdealProcessor: %hhu\n", pTEB->IdealProcessor);
+    printf("GuaranteedStackBytes: %lu\n", pTEB->GuaranteedStackBytes);
+    printf("ReservedForPerf: %p\n", pTEB->ReservedForPerf);
+    printf("ReservedForOle: %p\n", pTEB->ReservedForOle);
+    printf("WaitingOnLoaderLock: %lu\n", pTEB->WaitingOnLoaderLock);
+    printf("SavedPriorityState: %p\n", pTEB->SavedPriorityState);
+    printf("ReservedForCodeCoverage: %llu\n", pTEB->ReservedForCodeCoverage);
+    printf("ThreadPoolData: %p\n", pTEB->ThreadPoolData);
+    printf("TlsExpansionSlots: %p\n", pTEB->TlsExpansionSlots);
+#ifdef _WIN64
+    printf("DeallocationBStore: %p\n", pTEB->DeallocationBStore);
+    printf("BStoreLimit: %p\n", pTEB->BStoreLimit);
+#endif
+    printf("MuiGeneration: %lu\n", pTEB->MuiGeneration);
+    printf("IsImpersonating: %lu\n", pTEB->IsImpersonating);
+    printf("NlsCache: %p\n", pTEB->NlsCache);
+    printf("pShimData: %p\n", pTEB->pShimData);
+    printf("HeapData: %lu\n", pTEB->HeapData);
+    printf("ActiveFrame: %p\n", pTEB->ActiveFrame);
+    printf("CurrentTransactionHandle: %p\n", pTEB->CurrentTransactionHandle);
+    printf("FlsData: %p\n", pTEB->FlsData);
+    printf("PreferredLanguages: %p\n", pTEB->PreferredLanguages);
+    printf("UserPrefLanguages: %p\n", pTEB->UserPrefLanguages);
+    printf("MergedPrefLanguages: %p\n", pTEB->MergedPrefLanguages);
+    printf("MuiImpersonation: %lu\n", pTEB->MuiImpersonation);
+    printf("CrossTebFlags: %u\n", pTEB->CrossTebFlags);
+    printf("SpareCrossTebBits: %hu\n", pTEB->SpareCrossTebBits);
+    printf("SameTebFlags: %u\n", pTEB->SameTebFlags);
+    printf("SafeThunkCall: %hu\n", pTEB->SafeThunkCall);
+    printf("InDebugPrint: %hu\n", pTEB->InDebugPrint);
+    printf("HasFiberData: %hu\n", pTEB->HasFiberData);
+    printf("SkipThreadAttach: %hu\n", pTEB->SkipThreadAttach);
+    printf("WerInShipAssertCode: %hu\n", pTEB->WerInShipAssertCode);
+    printf("RanProcessInit: %hu\n", pTEB->RanProcessInit);
+    printf("ClonedThread: %hu\n", pTEB->ClonedThread);
+    printf("SuppressDebugMsg: %hu\n", pTEB->SuppressDebugMsg);
+    printf("DisableUserStackWalk: %hu\n", pTEB->DisableUserStackWalk);
+    printf("RtlExceptionAttached: %hu\n", pTEB->RtlExceptionAttached);
+    printf("InitialThread: %hu\n", pTEB->InitialThread);
+    printf("SessionAware: %hu\n", pTEB->SessionAware);
+    printf("LoadOwner: %hu\n", pTEB->LoadOwner);
+    printf("LoaderWorker: %hu\n", pTEB->LoaderWorker);
+    printf("SkipLoaderInit: %hu\n", pTEB->SkipLoaderInit);
+    printf("SkipFileAPIBrokering: %hu\n", pTEB->SkipFileAPIBrokering);
+    printf("TxnScopeEnterCallback: %p\n", pTEB->TxnScopeEnterCallback);
+    printf("TxnScopeExitCallback: %p\n", pTEB->TxnScopeExitCallback);
+    printf("TxnScopeContext: %p\n", pTEB->TxnScopeContext);
+    printf("LockCount: %lu\n", pTEB->LockCount);
+    printf("WowTebOffset: %ld\n", pTEB->WowTebOffset);
+    printf("ResourceRetValue: %p\n", pTEB->ResourceRetValue);
+    printf("ReservedForWdf: %p\n", pTEB->ReservedForWdf);
+    printf("ReservedForCrt: %llu\n", pTEB->ReservedForCrt);
+    WCHAR guidString[39];
+
+    int result = StringFromGUID2(&pTEB->EffectiveContainerId, guidString, sizeof(guidString) / sizeof(WCHAR));
+
+    if (result > 0) {
+        wprintf(L"EffectiveContainerId: %s\n", guidString);
+    }
+    else if (result == 0) {
+        printf("Failed to convert GUID to string: Buffer too small.\n");
+    }
+    else {
+        printf("Failed to convert GUID to string: Unknown error.\n");
+    }
+
+    printf("SpinCallCount: %lu\n", pTEB->SpinCallCount);
+    printf("ExtendedFeatureDisableMask: %llu\n", pTEB->ExtendedFeatureDisableMask);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+    UNREFERENCED_PARAMETER(lpReserved);
+    UNREFERENCED_PARAMETER(hModule);
+
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        AllocConsole();
+
+        FILE* pConsoleStream;
+        freopen_s(&pConsoleStream, "CONOUT$", "w", stdout);
+
+        PTEB pTEB = NtCurrentTeb();
+
+        printTEBFields(pTEB);
+
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        FreeConsole();
+        break;
+    }
+    return TRUE;
+}
---
+++ src/scn/struct.h
@@ -0,0 +1,518 @@
+#pragma once
+
+#include <Windows.h>
+
+/*
+typedef struct _TELEMETRY_COVERAGE_HEADER
+{
+    UCHAR MajorVersion;
+    UCHAR MinorVersion;
+    struct
+    {
+        USHORT TracingEnabled : 1;
+        USHORT Reserved1 : 15;
+    };
+    ULONG HashTableEntries;
+    ULONG HashIndexMask;
+    ULONG TableUpdateVersion;
+    ULONG TableSizeInBytes;
+    ULONG LastResetTick;
+    ULONG ResetRound;
+    ULONG Reserved2;
+    ULONG RecordedCount;
+    ULONG Reserved3[4];
+    ULONG HashTable[ANYSIZE_ARRAY];
+} TELEMETRY_COVERAGE_HEADER, * PTELEMETRY_COVERAGE_HEADER;
+
+typedef struct _ASSEMBLY_STORAGE_MAP_ENTRY
+{
+    ULONG Flags;
+    UNICODE_STRING DosPath;
+    HANDLE Handle;
+} ASSEMBLY_STORAGE_MAP_ENTRY, * PASSEMBLY_STORAGE_MAP_ENTRY;
+
+typedef struct _ASSEMBLY_STORAGE_MAP
+{
+    ULONG Flags;
+    ULONG AssemblyCount;
+    PASSEMBLY_STORAGE_MAP_ENTRY* AssemblyArray;
+} ASSEMBLY_STORAGE_MAP, * PASSEMBLY_STORAGE_MAP;
+*/
+
+typedef struct _TEB_ACTIVE_FRAME_CONTEXT
+{
+    ULONG Flags;
+    PSTR FrameName;
+} TEB_ACTIVE_FRAME_CONTEXT, * PTEB_ACTIVE_FRAME_CONTEXT;
+
+typedef struct _TEB_ACTIVE_FRAME
+{
+    ULONG Flags;
+    struct _TEB_ACTIVE_FRAME* Previous;
+    PTEB_ACTIVE_FRAME_CONTEXT Context;
+} TEB_ACTIVE_FRAME, * PTEB_ACTIVE_FRAME;
+
+typedef struct _API_SET_NAMESPACE
+{
+    ULONG Version;
+    ULONG Size;
+    ULONG Flags;
+    ULONG Count;
+    ULONG EntryOffset;
+    ULONG HashOffset;
+    ULONG HashFactor;
+} API_SET_NAMESPACE, * PAPI_SET_NAMESPACE;
+
+typedef struct _PEB_LDR_DATA
+{
+    ULONG Length;
+    BOOLEAN Initialized;
+    HANDLE SsHandle;
+    LIST_ENTRY InLoadOrderModuleList;
+    LIST_ENTRY InMemoryOrderModuleList;
+    LIST_ENTRY InInitializationOrderModuleList;
+    PVOID EntryInProgress;
+    BOOLEAN ShutdownInProgress;
+    HANDLE ShutdownThreadId;
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
+
+typedef struct _CLIENT_ID
+{
+    HANDLE UniqueProcess;
+    HANDLE UniqueThread;
+} CLIENT_ID, * PCLIENT_ID;
+
+typedef struct _ACTIVATION_CONTEXT* PACTIVATION_CONTEXT;
+
+typedef struct _ACTIVATION_CONTEXT_DATA
+{
+    ULONG Magic;
+    ULONG HeaderSize;
+    ULONG FormatVersion;
+    ULONG TotalSize;
+    ULONG DefaultTocOffset;
+    ULONG ExtendedTocOffset;
+    ULONG AssemblyRosterOffset;
+    ULONG Flags;
+} ACTIVATION_CONTEXT_DATA, * PACTIVATION_CONTEXT_DATA;
+
+typedef struct _RTL_ACTIVATION_CONTEXT_STACK_FRAME
+{
+    struct _RTL_ACTIVATION_CONTEXT_STACK_FRAME* Previous;
+    PACTIVATION_CONTEXT ActivationContext;
+    ULONG Flags;
+} RTL_ACTIVATION_CONTEXT_STACK_FRAME, * PRTL_ACTIVATION_CONTEXT_STACK_FRAME;
+
+typedef struct _ACTIVATION_CONTEXT_STACK
+{
+    PRTL_ACTIVATION_CONTEXT_STACK_FRAME ActiveFrame;
+    LIST_ENTRY FrameListCache;
+    ULONG Flags;
+    ULONG NextCookieSequenceNumber;
+    ULONG StackId;
+} ACTIVATION_CONTEXT_STACK, * PACTIVATION_CONTEXT_STACK;
+
+typedef struct _TELEMETRY_COVERAGE_HEADER
+{
+    UCHAR MajorVersion;
+    UCHAR MinorVersion;
+    struct
+    {
+        USHORT TracingEnabled : 1;
+        USHORT Reserved1 : 15;
+    };
+    ULONG HashTableEntries;
+    ULONG HashIndexMask;
+    ULONG TableUpdateVersion;
+    ULONG TableSizeInBytes;
+    ULONG LastResetTick;
+    ULONG ResetRound;
+    ULONG Reserved2;
+    ULONG RecordedCount;
+    ULONG Reserved3[4];
+    ULONG HashTable[ANYSIZE_ARRAY];
+} TELEMETRY_COVERAGE_HEADER, * PTELEMETRY_COVERAGE_HEADER;
+
+#define GDI_HANDLE_BUFFER_SIZE32 34
+#define GDI_HANDLE_BUFFER_SIZE64 60
+
+#ifndef _WIN64
+#define GDI_HANDLE_BUFFER_SIZE GDI_HANDLE_BUFFER_SIZE32
+#else
+#define GDI_HANDLE_BUFFER_SIZE GDI_HANDLE_BUFFER_SIZE64
+#endif
+typedef struct _RTL_USER_PROCESS_PARAMETERS* PRTL_USER_PROCESS_PARAMETERS;
+typedef struct _RTL_BITMAP* PRTL_BITMAP;
+typedef struct _SILO_USER_SHARED_DATA* PSILO_USER_SHARED_DATA;
+typedef struct _LEAP_SECOND_DATA* PLEAP_SECOND_DATA;
+typedef ULONG GDI_HANDLE_BUFFER[GDI_HANDLE_BUFFER_SIZE];
+
+typedef struct _PEB
+{
+    BOOLEAN InheritedAddressSpace;
+    BOOLEAN ReadImageFileExecOptions;
+    BOOLEAN BeingDebugged;
+    union
+    {
+        BOOLEAN BitField;
+        struct
+        {
+            BOOLEAN ImageUsesLargePages : 1;
+            BOOLEAN IsProtectedProcess : 1;
+            BOOLEAN IsImageDynamicallyRelocated : 1;
+            BOOLEAN SkipPatchingUser32Forwarders : 1;
+            BOOLEAN IsPackagedProcess : 1;
+            BOOLEAN IsAppContainer : 1;
+            BOOLEAN IsProtectedProcessLight : 1;
+            BOOLEAN IsLongPathAwareProcess : 1;
+        };
+    };
+
+    HANDLE Mutant;
+
+    PVOID ImageBaseAddress;
+    PPEB_LDR_DATA Ldr;
+    PRTL_USER_PROCESS_PARAMETERS ProcessParameters;
+    PVOID SubSystemData;
+    PVOID ProcessHeap;
+    PRTL_CRITICAL_SECTION FastPebLock;
+    PSLIST_HEADER AtlThunkSListPtr;
+    PVOID IFEOKey;
+
+    union
+    {
+        ULONG CrossProcessFlags;
+        struct
+        {
+            ULONG ProcessInJob : 1;
+            ULONG ProcessInitializing : 1;
+            ULONG ProcessUsingVEH : 1;
+            ULONG ProcessUsingVCH : 1;
+            ULONG ProcessUsingFTH : 1;
+            ULONG ProcessPreviouslyThrottled : 1;
+            ULONG ProcessCurrentlyThrottled : 1;
+            ULONG ProcessImagesHotPatched : 1;
+            ULONG ReservedBits0 : 24;
+        };
+    };
+    union
+    {
+        PVOID KernelCallbackTable;
+        PVOID UserSharedInfoPtr;
+    };
+    ULONG SystemReserved;
+    ULONG AtlThunkSListPtr32;
+    PAPI_SET_NAMESPACE ApiSetMap;
+    ULONG TlsExpansionCounter;
+    PRTL_BITMAP TlsBitmap;
+    ULONG TlsBitmapBits[2];
+
+    PVOID ReadOnlySharedMemoryBase;
+    PSILO_USER_SHARED_DATA SharedData;
+    PVOID* ReadOnlyStaticServerData;
+
+    PVOID AnsiCodePageData;
+    PVOID OemCodePageData;
+    PVOID UnicodeCaseTableData;
+
+    ULONG NumberOfProcessors;
+    ULONG NtGlobalFlag;
+
+    ULARGE_INTEGER CriticalSectionTimeout;
+    SIZE_T HeapSegmentReserve;
+    SIZE_T HeapSegmentCommit;
+    SIZE_T HeapDeCommitTotalFreeThreshold;
+    SIZE_T HeapDeCommitFreeBlockThreshold;
+
+    ULONG NumberOfHeaps;
+    ULONG MaximumNumberOfHeaps;
+    PVOID* ProcessHeaps;
+
+    PVOID GdiSharedHandleTable;
+    PVOID ProcessStarterHelper;
+    ULONG GdiDCAttributeList;
+
+    PRTL_CRITICAL_SECTION LoaderLock;
+
+    ULONG OSMajorVersion;
+    ULONG OSMinorVersion;
+    USHORT OSBuildNumber;
+    USHORT OSCSDVersion;
+    ULONG OSPlatformId;
+    ULONG ImageSubsystem;
+    ULONG ImageSubsystemMajorVersion;
+    ULONG ImageSubsystemMinorVersion;
+    KAFFINITY ActiveProcessAffinityMask;
+    GDI_HANDLE_BUFFER GdiHandleBuffer;
+    PVOID PostProcessInitRoutine;
+
+    PRTL_BITMAP TlsExpansionBitmap;
+    ULONG TlsExpansionBitmapBits[32];
+
+    ULONG SessionId;
+
+    ULARGE_INTEGER AppCompatFlags;
+    ULARGE_INTEGER AppCompatFlagsUser;
+    PVOID pShimData;
+    PVOID AppCompatInfo;
+
+    // UNICODE_STRING CSDVersion;
+
+    PACTIVATION_CONTEXT_DATA ActivationContextData;
+    // PASSEMBLY_STORAGE_MAP ProcessAssemblyStorageMap;
+    PACTIVATION_CONTEXT_DATA SystemDefaultActivationContextData;
+    // PASSEMBLY_STORAGE_MAP SystemAssemblyStorageMap;
+
+    SIZE_T MinimumStackCommit;
+
+    PVOID SparePointers[2];
+    PVOID PatchLoaderData;
+    PVOID ChpeV2ProcessInfo;
+
+    ULONG AppModelFeatureState;
+    ULONG SpareUlongs[2];
+
+    USHORT ActiveCodePage;
+    USHORT OemCodePage;
+    USHORT UseCaseMapping;
+    USHORT UnusedNlsField;
+
+    PVOID WerRegistrationData;
+    PVOID WerShipAssertPtr;
+
+    union
+    {
+        PVOID pContextData;
+        PVOID pUnused;
+        PVOID EcCodeBitMap; // WIN11
+    };
+
+    PVOID pImageHeaderHash;
+    union
+    {
+        ULONG TracingFlags;
+        struct
+        {
+            ULONG HeapTracingEnabled : 1;
+            ULONG CritSecTracingEnabled : 1;
+            ULONG LibLoaderTracingEnabled : 1;
+            ULONG SpareTracingBits : 29;
+        };
+    };
+    ULONGLONG CsrServerReadOnlySharedMemoryBase;
+    PRTL_CRITICAL_SECTION TppWorkerpListLock;
+    LIST_ENTRY TppWorkerpList;
+    PVOID WaitOnAddressHashTable[128];
+    PTELEMETRY_COVERAGE_HEADER TelemetryCoverageHeader;
+    ULONG CloudFileFlags;
+    ULONG CloudFileDiagFlags;
+    CHAR PlaceholderCompatibilityMode;
+    CHAR PlaceholderCompatibilityModeReserved[7];
+    PLEAP_SECOND_DATA LeapSecondData;
+    union
+    {
+        ULONG LeapSecondFlags;
+        struct
+        {
+            ULONG SixtySecondEnabled : 1;
+            ULONG Reserved : 31;
+        };
+    };
+    ULONG NtGlobalFlag2;
+    ULONGLONG ExtendedFeatureDisableMask; // since WIN11
+} PEB, * PPEB;
+
+typedef struct _UNICODE_STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    _Field_size_bytes_part_opt_(MaximumLength, Length) PWCH Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+#define GDI_BATCH_BUFFER_SIZE 310
+
+typedef struct _GDI_TEB_BATCH
+{
+    ULONG Offset;
+    ULONG_PTR HDC;
+    ULONG Buffer[GDI_BATCH_BUFFER_SIZE];
+} GDI_TEB_BATCH, * PGDI_TEB_BATCH;
+
+#define WIN32_CLIENT_INFO_LENGTH 62
+#define STATIC_UNICODE_BUFFER_LENGTH 261
+
+typedef struct _TEB
+{
+    NT_TIB NtTib;
+
+    PVOID EnvironmentPointer;
+    CLIENT_ID ClientId;
+    PVOID ActiveRpcHandle;
+    PVOID ThreadLocalStoragePointer;
+    PPEB ProcessEnvironmentBlock;
+
+    ULONG LastErrorValue;
+    ULONG CountOfOwnedCriticalSections;
+    PVOID CsrClientThread;
+    PVOID Win32ThreadInfo;
+    ULONG User32Reserved[26];
+    ULONG UserReserved[5];
+    PVOID WOW32Reserved;
+    LCID CurrentLocale;
+    ULONG FpSoftwareStatusRegister;
+    PVOID ReservedForDebuggerInstrumentation[16];
+#ifdef _WIN64
+    PVOID SystemReserved1[30];
+#else
+    PVOID SystemReserved1[26];
+#endif
+
+    CHAR PlaceholderCompatibilityMode;
+    BOOLEAN PlaceholderHydrationAlwaysExplicit;
+    CHAR PlaceholderReserved[10];
+
+    ULONG ProxiedProcessId;
+    ACTIVATION_CONTEXT_STACK ActivationStack;
+
+    UCHAR WorkingOnBehalfTicket[8];
+    NTSTATUS ExceptionCode;
+
+    PACTIVATION_CONTEXT_STACK ActivationContextStackPointer;
+    ULONG_PTR InstrumentationCallbackSp;
+    ULONG_PTR InstrumentationCallbackPreviousPc;
+    ULONG_PTR InstrumentationCallbackPreviousSp;
+#ifdef _WIN64
+    ULONG TxFsContext;
+#endif
+
+    BOOLEAN InstrumentationCallbackDisabled;
+#ifdef _WIN64
+    BOOLEAN UnalignedLoadStoreExceptions;
+#endif
+#ifndef _WIN64
+    UCHAR SpareBytes[23];
+    ULONG TxFsContext;
+#endif
+    GDI_TEB_BATCH GdiTebBatch;
+    CLIENT_ID RealClientId;
+    HANDLE GdiCachedProcessHandle;
+    ULONG GdiClientPID;
+    ULONG GdiClientTID;
+    PVOID GdiThreadLocalInfo;
+    ULONG_PTR Win32ClientInfo[WIN32_CLIENT_INFO_LENGTH];
+    PVOID glDispatchTable[233];
+    ULONG_PTR glReserved1[29];
+    PVOID glReserved2;
+    PVOID glSectionInfo;
+    PVOID glSection;
+    PVOID glTable;
+    PVOID glCurrentRC;
+    PVOID glContext;
+
+    NTSTATUS LastStatusValue;
+    UNICODE_STRING StaticUnicodeString;
+    WCHAR StaticUnicodeBuffer[STATIC_UNICODE_BUFFER_LENGTH];
+
+    PVOID DeallocationStack;
+    PVOID TlsSlots[TLS_MINIMUM_AVAILABLE];
+    LIST_ENTRY TlsLinks;
+
+    PVOID Vdm;
+    PVOID ReservedForNtRpc;
+    PVOID DbgSsReserved[2];
+
+    ULONG HardErrorMode;
+#ifdef _WIN64
+    PVOID Instrumentation[11];
+#else
+    PVOID Instrumentation[9];
+#endif
+    GUID ActivityId;
+
+    PVOID SubProcessTag;
+    PVOID PerflibData;
+    PVOID EtwTraceData;
+    PVOID WinSockData;
+    ULONG GdiBatchCount;
+
+    union
+    {
+        PROCESSOR_NUMBER CurrentIdealProcessor;
+        ULONG IdealProcessorValue;
+        struct
+        {
+            UCHAR ReservedPad0;
+            UCHAR ReservedPad1;
+            UCHAR ReservedPad2;
+            UCHAR IdealProcessor;
+        };
+    };
+
+    ULONG GuaranteedStackBytes;
+    PVOID ReservedForPerf;
+    PVOID ReservedForOle; // tagSOleTlsData
+    ULONG WaitingOnLoaderLock;
+    PVOID SavedPriorityState;
+    ULONG_PTR ReservedForCodeCoverage;
+    PVOID ThreadPoolData;
+    PVOID* TlsExpansionSlots;
+#ifdef _WIN64
+    PVOID DeallocationBStore;
+    PVOID BStoreLimit;
+#endif
+    ULONG MuiGeneration;
+    ULONG IsImpersonating;
+    PVOID NlsCache;
+    PVOID pShimData;
+    ULONG HeapData;
+    HANDLE CurrentTransactionHandle;
+    PTEB_ACTIVE_FRAME ActiveFrame;
+    PVOID FlsData;
+
+    PVOID PreferredLanguages;
+    PVOID UserPrefLanguages;
+    PVOID MergedPrefLanguages;
+    ULONG MuiImpersonation;
+
+    union
+    {
+        USHORT CrossTebFlags;
+        USHORT SpareCrossTebBits : 16;
+    };
+    union
+    {
+        USHORT SameTebFlags;
+        struct
+        {
+            USHORT SafeThunkCall : 1;
+            USHORT InDebugPrint : 1;
+            USHORT HasFiberData : 1;
+            USHORT SkipThreadAttach : 1;
+            USHORT WerInShipAssertCode : 1;
+            USHORT RanProcessInit : 1;
+            USHORT ClonedThread : 1;
+            USHORT SuppressDebugMsg : 1;
+            USHORT DisableUserStackWalk : 1;
+            USHORT RtlExceptionAttached : 1;
+            USHORT InitialThread : 1;
+            USHORT SessionAware : 1;
+            USHORT LoadOwner : 1;
+            USHORT LoaderWorker : 1;
+            USHORT SkipLoaderInit : 1;
+            USHORT SkipFileAPIBrokering : 1;
+        };
+    };
+
+    PVOID TxnScopeEnterCallback;
+    PVOID TxnScopeExitCallback;
+    PVOID TxnScopeContext;
+    ULONG LockCount;
+    LONG WowTebOffset;
+    PVOID ResourceRetValue;
+    PVOID ReservedForWdf;
+    ULONGLONG ReservedForCrt;
+    GUID EffectiveContainerId;
+    ULONGLONG LastSleepCounter; // since Windows 11
+    ULONG SpinCallCount;
+    ULONGLONG ExtendedFeatureDisableMask;
+} TEB, * PTEB;
